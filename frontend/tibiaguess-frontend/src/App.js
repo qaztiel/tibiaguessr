c61c7efd13252e6dbbddb1b89183a94ea4037848
josephaorahim <josephaorahim@yahoo.com>
2022-11-22 22:01:28 -0700

    got the tibia imported as leaflet overlay

--- frontend/tibiaguess-frontend/src/App.js
+++ frontend/tibiaguess-frontend/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Marker, ImageOverlay, LatLngBounds } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 
 function App() {
+  const position = [51.505, -0.09]
+  const bound = [[0,0], [40.773941, -74.12544]];
   return (
     <div id="map">
-      <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-      </MapContainer>
+        <ImageOverlay
+          url={"https://tibiamaps.github.io/tibia-map-data/floor-07-map.png"}
+          bounds={bound}
+          opacity={0.5}
+          zIndex={1000}
+        />
+  </MapContainer>
     </div>
     
   );
