dc5d653c568e5d8135443d4c94f85e2a5a904893
josephaorahim <josephaorahim@yahoo.com>
2022-11-26 21:30:55 -0700

    moving files around and adding component files and updating tibia

--- frontend/tibiaguess-frontend/src/components/TibiaMap.js
+++ frontend/tibiaguess-frontend/src/components/TibiaMap.js
@@ -3,19 +3,35 @@ import { MapContainer, TileLayer, Popup, Marker, ImageOverlay, LatLngBounds } fr
 
 export function TibiaMap() {
     const position = [0,0]
-    const bound = [
-    [-200,-200], 
-    [200, 200]
-    ];
     
+    // TODO: image bound needs work
+    const bound = [
+        [-250,-200], 
+        [200, 200]
+    ];
+
+    // TODO: bounds need work to keep from panning too far from map
+    const bounds = [
+        [-350, -250], 
+        [350, 250]
+    ];
     return (
         <div id="map">
-            <MapContainer center={position} zoom={2} scrollWheelZoom={true}>
-                <ImageOverlay
-                    url={"https://tibiamaps.github.io/tibia-map-data/floor-07-map.png"}
-                    bounds={bound}
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
+            <MapContainer 
+                center={position} 
+                zoom={3} 
+                scrollWheelZoom={true}
+                maxZoom={6}
+                minZoom={2}
+                trackResize={true}
+                maxBounds={bounds}
+                maxBoundsViscosity={1.0}
+                >
+                    <ImageOverlay
+                        url={"https://tibiamaps.github.io/tibia-map-data/floor-07-map.png"}
+                        bounds={bound}
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href= "https://www.tibiamaps.io">TibiaMaps.io</a>'
+                    />
             </MapContainer>
         </div>
     );
