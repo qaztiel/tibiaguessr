ae14c8e71b37a99e2afe2d9d02d8ff461938e843
josephaorahim <josephaorahim@yahoo.com>
2022-11-26 20:31:51 -0700

    removed png and refactored tibiamap into compenent

---
+++ frontend/tibiaguess-frontend/src/components/TibiaMap.js
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer, Popup, Marker, ImageOverlay, LatLngBounds } from 'react-leaflet'
+
+
+export function TibiaMap() {
+    const position = [0,0]
+    const bound = [
+    [-200,-200], 
+    [200, 200]
+    ];
+    
+    return (
+        <div id="map">
+            <MapContainer center={position} zoom={2} scrollWheelZoom={true}>
+                <ImageOverlay
+                    url={"https://tibiamaps.github.io/tibia-map-data/floor-07-map.png"}
+                    bounds={bound}
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+    );
+}
