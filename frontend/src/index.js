c6d9d04a701711771e12171067220caf48958c54
josephaorahim <josephaorahim@yahoo.com>
2022-12-07 21:28:31 -0700

    setting background color of body to pink

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
