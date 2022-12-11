99099bf6de0e8076ee293960ae23e9d9a1df8d5f
josephaorahim <josephaorahim@yahoo.com>
2022-12-11 15:29:36 -0700

    moved navbar socials to footer file

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,6 +6,20 @@ function Footer() {
   return (
     <>
       <footer>
+        {/* <div className='nav--social--wrapper'>
+          <a  href="https://github.com/qaztiel/tibiaguessr">
+            <i className='fa-brands fa-github fa-2x'></i>
+          </a>
+          <a href="https://www.tiktok.com/@tibiaguessr">
+            <i className='fa-brands fa-tiktok fa-2x'></i>
+          </a>
+          <a href="https://www.instagram.com/tibiaguessr/">
+            <i className='fa-brands fa-instagram fa-2x'></i>
+          </a>
+          <a href="https://twitter.com/tibiaguessr">
+            <i className='fa-brands fa-twitter fa-2x'></i>
+          </a>
+        </div> */}
           <button className='footer--btn'>Guess</button>
       </footer>
     </>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,20 +7,7 @@ function NavBar() {
     <>
       <nav>
         
-        {/* <div className='nav--social--wrapper'>
-          <a  href="https://github.com/qaztiel/tibiaguessr">
-            <i className='fa-brands fa-github fa-2x'></i>
-          </a>
-          <a href="https://www.tiktok.com/@tibiaguessr">
-            <i className='fa-brands fa-tiktok fa-2x'></i>
-          </a>
-          <a href="https://www.instagram.com/tibiaguessr/">
-            <i className='fa-brands fa-instagram fa-2x'></i>
-          </a>
-          <a href="https://twitter.com/tibiaguessr">
-            <i className='fa-brands fa-twitter fa-2x'></i>
-          </a>
-        </div> */}
+        
         
         <div className='nav--items'>
           <div className='logo'></div>
