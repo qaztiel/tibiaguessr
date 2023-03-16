5b1889a7b88802cb4f04eaddd0e64ba58ce0c50b
josephaorahim <josephaorahim@yahoo.com>
2023-03-16 13:35:53 -0700

    moving marker to its own component file

--- src/components/TibiaMap.js
+++ src/components/TibiaMap.js
@@ -46,21 +46,3 @@ export function TibiaMap() {
 }
 
 
-function LocationMarker() {
-    const [position, setPosition] = useState(null)
-    const map = useMapEvents({
-      click() {
-        map.locate()
-      },
-      locationfound(e) {
-        setPosition(e.latlng)
-        map.flyTo(e.latlng, map.getZoom())
-      },
-    })
-  
-    return position === null ? null : (
-      <Marker position={position}>
-        <Popup>You are here</Popup>
-      </Marker>
-    )
-  }
